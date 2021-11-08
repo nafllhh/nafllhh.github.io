ae05c74da28c414e9db1fca322a40e78a322806c
nafllhh <naufalendwin7@gmail.com>
2021-11-08 22:09:35 +0700

    add search-content

--- js/index.js
+++ js/index.js
@@ -6,3 +6,20 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+function search() {
+var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('input');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+} 
