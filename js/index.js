b7f9f182cbf1c1a754a2a275546bcbeeed713052
nafllhh <naufalendwin7@gmail.com>
2021-12-03 21:09:31 +0700

    back

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "400px";
+  document.getElementById("mySidenav").style.width = "390px";
 }
 /* Set the width of the side navigation to 0 */
 function closeNav() {
