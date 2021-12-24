a52781d67403be8e117bc0eaedd0a4739854fc80
nafllhh <naufalendwin7@gmail.com>
2021-12-24 17:59:19 +0700

    neovim <3

--- js/learn.js
+++ js/learn.js
@@ -1,25 +1,25 @@
 function clickk() {
-	document.getElementById("me").innerHTML= "ubah dong";
-  }
-  function al(){
-	alert("halo");
-  }
-  function size(){
-	document.getElementById('demo').style.fontSize='35px';
-	  document.getElementById('chn').style.fontSize='40px';
-	  
-  }
-  function box(){
-	document.getElementById('box').style.width="100px";
-	document.getElementById('box').style.height='100px';
-	document.getElementById('box').style.display="block";
-	document.getElementById('box').innerHTML="hha";
+  document.getElementById("me").innerHTML = "ubah dong";
+}
+function al() {
+  alert("halo");
+}
+function size() {
+  document.getElementById('demo').style.fontSize = '35px';
+  document.getElementById('chn').style.fontSize = '40px';
 
-  }
-  function clsbox(){
-	document.getElementById('box').style.width="0";
-	document.getElementById('box').style.height='0';
-	document.getElementById('box').style.display="none";
+}
+function box() {
+  document.getElementById('box').style.width = "100px";
+  document.getElementById('box').style.height = '100px';
+  document.getElementById('box').style.display = "block";
+  document.getElementById('box').innerHTML = "hha";
 
-  }
-  
+}
+function clsbox() {
+  document.getElementById('box').style.width = "0";
+  document.getElementById('box').style.height = '0';
+  document.getElementById('box').style.display = "none";
+
+}
+
